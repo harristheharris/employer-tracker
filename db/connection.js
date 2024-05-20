72efe209897c04dffb944ee120faff11a2bde5d2
harristheharris <anthonyharris7777@gmail.com>
2024-05-19 21:36:02 -0400

    creates connection and made each table in our schema

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,15 @@
 //establish connections with database
 
 //import mysql
+const mysql = require("mysql2")
 
 //create connection with our database
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: '' //whatever we name our database
+})
+
+module.exports = db;
